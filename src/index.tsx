d477f2f106cb1bba3e246e78ae6de7e4e829228a
Ardian Shala <ardian.sh@outlook.de>
2021-09-27 09:39:36 +0200

    packages upgraded

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { render } from "react-dom";
 import { App } from "./App";
 import "./style.scss";
 
-render(<App />, document.body);
+render(
+	<Suspense fallback={<div>Loading...</div>}>
+		<React.StrictMode>
+			<App />
+		</React.StrictMode>
+	</Suspense>,
+	document.getElementById('root')
+);
 
 if (module.hot) {
 	module.hot.accept();
